654f3f615e62332e0c7c483a96a33946f45717bd
Chris Timms <chriscotimms@gmail.com>
2023-06-13 19:59:49 +0100

    just starting

---
+++ oddnumber_6kyu.js
@@ -0,0 +1,40 @@
+function findOdd(A) {
+    
+
+
+    /*
+    A ='hello';
+    var re = new RegExp(A[0], "gi");
+    console.log(re);
+    var mat = A.match(re);
+    console.log(mat);
+    */
+   
+    /*
+    let len = A.length;  
+    for (let i=0;i<len;i+=1){
+  
+        for (let j=0;j<len;j+=1){
+            if (A[i] === A[j]){
+                console.log(A[i]);
+            }  else {
+                
+            }
+        }
+        
+    }
+    */
+
+    //return 0;
+  }
+
+
+  findOdd([20,1,20, 1])//, 5);
+  /*
+  findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])//, 5);
+  findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])//, -1);
+  findOdd([20,1,1,2,2,3,3,5,5,4,20,4,5])//, 5);
+  findOdd([10])//, 10);
+  findOdd([1,1,1,1,1,1,10,1,1,1,1])//, 10);
+  findOdd([5,4,3,2,1,5,4,3,2,10,10])//, 1)
+  */
