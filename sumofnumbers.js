b69f2d54d1a3916bb3a27658af214dcc1c4e2ce9
Chris Timms <chriscotimms@gmail.com>
2023-06-13 12:29:02 +0100

    working range to start

---
+++ sumofnumbers.js
@@ -0,0 +1,20 @@
+function getSum(a, b){
+    let range = Math.abs(a - b);
+    
+}
+    /*
+    let sum = 0;
+    let x = x.fill()
+    let len = x.length;
+    for (let i=0;i<len;i+=1) {
+        sum += x[i]; 
+    }
+    console.log(sum);
+    return sum;
+}
+*/
+
+getSum(-5, -2);
+//getSum(1, 0);
+//getSum(1, 2);
+//getSum(1, 1);
